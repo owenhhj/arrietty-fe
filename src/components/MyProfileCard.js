6a9326a8dbf9ac6c1d5622de778b8989d908d692
h <juanjuanjks@163.com>
2022-02-15 15:12:49 +0800

    second commit

---
+++ src/components/MyProfileCard.js
@@ -0,0 +1,16 @@
+import './MyProfileCard.css'
+
+function MyProfileCard(props) {
+
+  return (
+    <div className="MyProfileCard">
+      <h2>{props.title}</h2>
+    </div>
+  )
+}
+
+MyProfileCard.defaultProps = {
+  title: "My Profile Card"
+}
+
+export default MyProfileCard
