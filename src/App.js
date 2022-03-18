815b68ff7b3b1b8540b223a6fd0f749a5c59786a
h <juanjuanjks@163.com>
2022-03-18 21:41:19 +0800

    dragndrop plus deletion, middlepoint commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MyProfile from "./components/MyProfile";
 import Navbar from "./components/Navbar";
 import AddNewAd from "./components/AddNewAd";
+import AddNewAdParent from "./components/AddNewAdParent";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AdminTagInputWidget from "./components/admin/AdminTagInputWidget";
 import SelectSearch, {fuzzySearch} from "react-select-search";
@@ -48,7 +49,9 @@ function App() {
         </BrowserRouter>
 
         {/*<MyProfile/>*/}
-        <AddNewAd/>
+        {/*<AddNewAd/>*/}
+        <AddNewAdParent/>
+
         {/*<AdminTagInputWidget/>*/}
     </div>
   );
--- src/components/AddNewAd.js
+++ src/components/AddNewAd.js
@@ -4,81 +4,110 @@ import {useState} from 'react'
 import {dataFetch} from "./common/common";
 
 
-// TODO component logic:
-// select single/multiple files --> 'Add' to put into draganddrop and sort --> 'Submit' to post
-function AddNewAd(props) {
-  const ROOT = 'http://localhost:8000/'
-  // const bookCovers = ["bookcover1.jpg", "bookcover2.jpg", "bookcover3.jpg", "bookcover4.jpg"]  // samples
-  const grid = 6
-  const [picURLs, setPicURLs] = useState([])
-  const [formData, setFormData] = useState(new FormData())  // form has to be a state, pic not state still works
-  let pic = []  // current pic being added
+function AddNewAd({
+  toParent
+                  }) {
+  const ROOT = 'http://localhost:8000/';
+  // const bookCovers = ["bookcover1.jpg", "bookcover2.jpg", "bookcover3.jpg", "bookcover4.jpg"];  // samples
+  const grid = 6;
+  // TODO current implementation: send to parent before setState()
+  const [picURLs, setPicURLs] = useState([]);
+  // const [formData, setFormData] = useState(new FormData());  // form has to be a state, pic not state still works
+  const [picHover, setPicHover] = useState(false);
+  let pic = [];  // current pic being added
 
   const picInputChange = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (e.target.files && e.target.files[0]) {
       for (let i=0; i<e.target.files.length; i++) {
         // TODO upload files with the same name?
         // TODO upload files .jpg/png check
         // TODO release RAM from createObjectURL after submission
-        pic.push(
-          {"name": e.target.files[i].name, "url": URL.createObjectURL(e.target.files[i])}
-        )
-        formData.append("files", e.target.files[i])
+        pic.push({
+          "name": e.target.files[i].name,
+          "url": URL.createObjectURL(e.target.files[i]),
+          "file": e.target.files[i]
+        });
+        // formData.append("files", e.target.files[i])
       }
-      setFormData(formData)
+      // setFormData(formData)
     }
   }
 
   const handlePicAdd = (e) => {
-    e.preventDefault()
-    let legal = pic.length > 0
+    e.preventDefault();
+    let legal = pic.length > 0;
     picURLs.forEach((uploaded) => {
       pic.forEach((newPic) => {
         if (newPic.name === uploaded.name) {
-          legal = false
+          legal = false;
         }
       })
     })
     if (legal) {
-      setPicURLs([...picURLs, ...pic])
-      pic = []
+      toParent([...picURLs, ...pic].map((item) => (item.file)));
+      setPicURLs([...picURLs, ...pic]);
+      pic = [];
     } else {
-      pic = []
-      alert('Upload another picture!')
+      pic = [];
+      alert('Upload another picture!');
     }
   }
 
+  // TODO two func below used to toggle state of individual picHover instead of global picHover
+  // const handlePicMouseEnter = (e) => {
+  //   e.preventDefault();
+  //   console.log(e);
+  // }
+  //
+  // const handlePicMouseLeave = (e) => {
+  //   e.preventDefault();
+  //   console.log(e);
+  // }
+
+  // TODO bug: re-upload deleted file can cause illegal
+  const handlePicDelete = (e) => {
+    e.preventDefault();
+    picURLs.splice(Number(e.target.name), 1);
+    toParent(picURLs.map((item) => (item.file)));
+    setPicURLs(picURLs);
+  }
+
   // TODO redo sorting for better performance
-  // TODO pass to parent for API send
   const handleAdSubmit = (e) => {
-    e.preventDefault()
-    let newForm = new FormData()
-    picURLs.forEach((target) => {
-      formData.getAll('files').forEach((f) => {
-        if (f.name === target.name) {
-          newForm.append('files', f)
-        }
-      })
-    })
-    let body = {
-      "id": null, "isTextbook": true, "tagId": 12, "images": newForm, "price": 200, "comment": "no comment"
-    }
-    dataFetch(
-      ROOT+'advertisement?action=update',
-      {method: 'POST', body: body},
-      (res) => {
-        console.log("clear all states & createObjectURLs on this page", res)
-      },
-      (err) => {
-        console.log("keep this page and resubmit", err)
-      }
-    )
+    e.preventDefault();
+    toParent(picURLs.map((item) => (item.file)))
+
+    // let newForm = new FormData();
+    // let toParent = [];
+    // picURLs.forEach((target) => {
+    //   formData.getAll('files').forEach((f) => {
+    //     if (f.name === target.name) {
+    //       newForm.append('files', f);
+    //       toParent.push(f)
+    //     }
+    //   })
+    // })
+    // callback(toParent)
+    // let body = {
+    //   "id": null, "isTextbook": true, "tagId": 12, "images": newForm, "price": 200, "comment": "no comment"
+    // }
+    // dataFetch(
+    //   ROOT+'advertisement?action=update',
+    //   {method: 'POST', body: body},
+    //   (res) => {
+    //     console.log("clear all states & createObjectURLs on this page", res)
+    //   },
+    //   (err) => {
+    //     console.log("keep this page and resubmit", err)
+    //   }
+    // )
   }
   
   const getItemStyle = (isDragging, draggableStyle) => ({
     userSelect: 'none',
-    padding: grid * 2,
+    // padding: grid * 2,
+    padding: grid,
     margin: `0 ${grid}px 0 0`,
     background: isDragging ? 'lightgreen' : 'grey',
     ...draggableStyle,
@@ -98,6 +127,7 @@ function AddNewAd(props) {
     return result
   }
 
+  // this function setState --> picURL and send to parent
   const onDragEnd = (result) => {
     // dropped outside the list
     if (!result.destination) {return}
@@ -105,8 +135,9 @@ function AddNewAd(props) {
       picURLs,
       result.source.index,
       result.destination.index
-    )
-    setPicURLs(items)
+    );
+    toParent(items.map((item) => (item.file)));
+    setPicURLs(items);
   }
 
   return (
@@ -123,6 +154,8 @@ function AddNewAd(props) {
           {(provided, snapshot) => (
             <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)} {...provided.droppableProps}>
 
+              {/*TODO map the delete icon here, use onHover where?*/}
+              {/*TODO one single icon that floats with pics or one each?*/}
               {picURLs.map((item, index) => (
                 <Draggable key={item.name} draggableId={item.name} index={index}>
                   {(provided, snapshot) => {return (
@@ -131,7 +164,17 @@ function AddNewAd(props) {
                         snapshot.isDragging,
                         provided.draggableProps.style)}
                     >
-                      <img key={item.name} src={item.url} alt=""/>
+                      {/*TODO new div to hold img and delete btn*/}
+                      <div
+                        className="DragContainer" name={index.toString()}
+                        onMouseEnter={()=>{setPicHover(true)}}
+                        onMouseLeave={()=>{setPicHover(false)}}
+                      >
+                        <img key={index.toString()} src={item.url} alt=""/>
+                        {picHover && <button className="PicDeleteBtn" name={index.toString()} onClick={handlePicDelete}>x</button>}
+                      </div>
+
+
                     </div>
                   )}}
                 </Draggable>
@@ -148,4 +191,4 @@ function AddNewAd(props) {
   )
 }
 
-export default AddNewAd
+export default AddNewAd;
