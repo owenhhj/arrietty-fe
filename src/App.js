595a4208893ac5b2adc6335331004dd4b59af520
h <juanjuanjks@163.com>
2022-03-24 20:16:38 +0800

    GeneralNoti finalized, may be merged

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import MyProfile from "./components/MyProfile";
 import Navbar from "./components/Navbar";
@@ -22,7 +22,7 @@ function App() {
 
         {/*<MyProfile/>*/}
         {/*<AdminTagInputWidget/>*/}
-        <AdvertisementUploadForm/>
+        {/*<AdvertisementUploadForm/>*/}
 
     </div>
   );
