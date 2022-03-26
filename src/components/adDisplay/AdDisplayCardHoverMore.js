d79bf34fc3b2ff2b75a08a53b0c1e4ab587c9d27
h <juanjuanjks@163.com>
2022-03-27 00:18:27 +0800

    AdDisplayCard --> hover book info

--- src/components/adDisplay/AdDisplayCardHoverMore.js
+++ src/components/adDisplay/AdDisplayCardHoverMore.js
@@ -1,12 +1,12 @@
-import './AdDisplayCard.css';
+import './AdDisplayCardHoverMore.css';
 
 function AdDisplayCardHoverMore({
-  title='caLcuLus',
+  textbookTitle='caLcuLus',
   isbn='123-456789101112',
   author='Haojia He',
   publisher='JinQiao',
   edition='3rd edition',
-  oprice=100,
+  originalPrice=100,
   relatedCourse='MATHSHU-101'
                                 }) {
 
@@ -15,7 +15,16 @@ function AdDisplayCardHoverMore({
 
   return (
     <div className={'AdDisplayCardHoverMore'}>
-      <p>sdaf</p>
+      <div className={'fields-container'}>
+        <p><span>Title: </span>{textbookTitle}</p>
+        <p><span>ISBN: </span>{isbn}</p>
+        <p><span>Author: </span>{author}</p>
+        <p><span>Publisher: </span>{publisher}</p>
+        <p><span>Edition: </span>{edition}</p>
+        <p><span>Price: </span>{originalPrice} RMB</p>
+        <p><span>Related: </span>{relatedCourse}</p>
+      </div>
+
     </div>
   );
 }
