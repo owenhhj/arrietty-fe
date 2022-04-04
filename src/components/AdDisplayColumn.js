578661cefaa1c10d83bd4fcb2c82f6b0a11060e3
h <juanjuanjks@163.com>
2022-04-04 10:36:01 +0800

    adListing: done auto load more on scrolling

--- src/components/AdDisplayColumn.js
+++ src/components/AdDisplayColumn.js
@@ -3,35 +3,20 @@ import SearchBar from "./searchBar/SearchBar";
 import AdDisplayCard from "./adDisplay/AdDisplayCard";
 import {dataFetch} from "./common/common";
 
-const ads = [
-  {id: 0, username: 'user1', userNetId: 'aa1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling This!!!', textbookTitle: 'Calculus', isbn: '123-4564033823', author: 'Owen', publisher: 'Owens Pub', edition: '3', originalPrice: '998', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:50, comment:'nothing to tell really', createdTime: null},
-  {id: 1, username: 'user2', userNetId: 'bb1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Buying This!!!', textbookTitle: 'sdrfg', isbn: '123-4564033823', author: 'Robert', publisher: 'Owens Pub', edition: '3', originalPrice: '3456', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_ad_preview_image.jpg', price:234, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-  {id: 2, username: 'user3', userNetId: 'cc1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling Buying!!!', textbookTitle: 'Calargculus', isbn: '123-4564033823', author: 'Haha', publisher: 'Owens Pub', edition: '3', originalPrice: '23', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:34, comment:'nothing to tell really', createdTime: null},
-]
+// const ads = [
+//   {id: 0, username: 'user1', userNetId: 'aa1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Selling This!!!', textbookTitle: 'Calculus', isbn: '123-4564033823', author: 'Owen', publisher: 'Owens Pub', edition: '3', originalPrice: '998', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_cover.jpg', price:50, comment:'nothing to tell really', createdTime: null},
+//   {id: 1, username: 'user2', userNetId: 'bb1111', userAvatarImageId: './avatar.jpg', adType: 'textbook', adTitle: 'Buying This!!!', textbookTitle: 'sdrfg', isbn: '123-4564033823', author: 'Robert', publisher: 'Owens Pub', edition: '3', originalPrice: '3456', relatedCourse: 'CSCISHU 101', otherTag: null, imageIds: './default_ad_preview_image.jpg', price:234, comment:'nothing to tell really', createdTime: null},
+// ]
 
 function AdDisplayColumn() {
   const ROOT = 'https://localhost:8000/';
-  let scrolling = false;
-  let lastScrollPos = 0;
-  const [adData, setAdData] = useState([]);
-  // const [queryBody, setQueryBody] = useState({
-  //   'adType': 'textbook',
-  //   'keyword': 'textbook1',  // todo pending back-end accept null
-  //   'pageNum': 0
-  // });
-  let queryBody = {
+  const [adData, setAdData] = useState([]);  // adListing data
+  const [isLoading, setIsLoading] = useState(0);  // index --> loadingMsg[]
+  const [queryBody, setQueryBody] = useState({  // save previous query for loadMore
     'adType': 'textbook',
-    'keyword': 'textbook1',  // todo pending back-end accept null
     'pageNum': 0
-  };
+  });
+  const loadingMsg = ['', 'Loading...', 'No more advertisements...'];
 
   useEffect(() => {
     console.log('onMount search with:', queryBody);
@@ -44,35 +29,37 @@ function AdDisplayColumn() {
       },
       (r) => {
         setAdData(r);
-        // setQueryBody({...queryBody, pageNum: 1});
-        queryBody.pageNum = 1;
+        let temp = queryBody;
+        temp.pageNum++;
+        setQueryBody(temp);
       },
       null
       );
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
   }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) {
+      handleLoadMore();
+    }
+  }, [isLoading]);
 
   const handleScroll = () => {
     if (document.body.offsetHeight-window.scrollY-window.innerHeight < 5) {
-      window.removeEventListener('scroll', handleScroll);
-      // console.log(window.scrollY, document.body.offsetHeight, window.innerHeight);
-      handleLoadMore();
-      console.log('inside Scroll after loading', queryBody.pageNum);
-      setTimeout(() => {
-        window.addEventListener('scroll', handleScroll);
-      }, 2000);
+      setIsLoading(1);
+    } else if (document.body.offsetHeight-window.scrollY-window.innerHeight > 100) {
+      setIsLoading(0);
     }
   }
 
   const handleSearchBar = (e) => {  // user search --> reset queryBody & queryPageNum
+    // setIsLoading(false);
     let temp = {...e, pageNum: 0};
-    // setQueryBody(temp);
-    queryBody = temp;
+    setQueryBody(temp);
     console.log('user search with:', temp);
     dataFetch(
       `${ROOT}search`,
@@ -83,8 +70,8 @@ function AdDisplayColumn() {
       },
       (r) => {
         setAdData(r);
-        // setQueryBody({...queryBody, pageNum: queryBody.pageNum+1});
-        queryBody = {...queryBody, pageNum: queryBody.pageNum+1};
+        temp.pageNum++;
+        setQueryBody(temp);
       },
       (e) => {
         console.warn(e);
@@ -103,18 +90,22 @@ function AdDisplayColumn() {
         body: JSON.stringify(queryBody)
       },
       (r) => {
-        console.log(r.length)
+        if (r.length === 0) {  // show 'no more ads'
+          setIsLoading(2);
+          return;
+        }
         let temp = adData;
         r.forEach(item => {
           temp.push(item);
         })
-        console.log('temp',temp.length);
         setAdData(temp)
-        // setAdData([...adData, ...r]);
-        // setQueryBody({...queryBody, pageNum: queryBody.pageNum+1});
-        queryBody.pageNum+=1;
+        temp = queryBody;  // save this query for loading more
+        temp.pageNum++;
+        setQueryBody(temp);
+        setIsLoading(0);
       },
       (e) => {
+        setIsLoading(0);
         console.warn(e);
       }
     )
@@ -132,13 +123,8 @@ function AdDisplayColumn() {
         );
       })}
 
-      <div>
-        <button onClick={handleLoadMore}>LoadMore</button>
-      </div>
-
-
       <div style={{width: 'auto', height:'300px'}}>
-
+        {isLoading!==0 && <div><p>{loadingMsg[isLoading]}</p></div>}
       </div>
     </div>
   );
