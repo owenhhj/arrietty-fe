32eada3fa7226e6c0d2becc7fe2bf05ed1eed9a5
h <juanjuanjks@163.com>
2022-04-09 00:16:42 +0800

    Notification: starting to dev

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import MyProfile from "./components/myProfile/MyProfile";
 import Navbar from "./components/Navbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import AdminTagInputWidget from "./components/admin/AdminTagInputWidget";
 import SelectSearch, {fuzzySearch} from "react-select-search";
 import "./components/common/select-search.css"
-import AdDisplayColumn from "./components/adDisplay/AdDisplayColumn";
-import AdUploadForm from "./components/adUploadForm/AdUploadForm";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
-import Modal from "react-modal";
+
 import MyPosts from "./components/MyPosts";
+import Notification from "./components/Notification";
+import Modal from "react-modal";
 
 
 
@@ -27,6 +25,7 @@ function App() {
                 <Route  path="/home" element={<Home/>} exact/>
                 <Route  path="/myPosts" element={<MyPosts/>} exact/>
                 <Route  path="/admin" element={<Admin/>} exact/>
+                <Route  path="/notification" element={<Notification/>} exact/>
 
             </Routes>
         </BrowserRouter>
