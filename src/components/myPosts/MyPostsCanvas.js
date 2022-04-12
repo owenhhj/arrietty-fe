b0befa32ae996401f5d150af8a1cb7d9c12e37d1
h <juanjuanjks@163.com>
2022-04-13 00:19:23 +0800

    myAdCard: halfway imp edit/delete

--- src/components/myPosts/MyPostsCanvas.js
+++ src/components/myPosts/MyPostsCanvas.js
@@ -1,13 +1,40 @@
-
-
+import MyPostsCard from "./MyPostsCard";
+import {dataFetch} from "../common/common";
+import {showGeneralNoti} from "../common/GeneralNotiProvider";
 
 function MyPostsCanvas() {
+  const ROOT = 'https://localhost:8000/';
 
+  const dispatch = showGeneralNoti();
 
+  const handleCallbackEdit = (id) => {
+    console.log('callbackEdit')
+  }
+
+  const handleCallbackDelete = (id) => {
+    console.log('callbackDelete')
+    dataFetch(
+      `${ROOT}advertisement?action=delete`,
+      {method: 'POST', body: {id: id}},
+      (res) => {
+        dispatch({action: "add", body: {msg: 'Ad deletion success', good: true}});
+        refreshData();
+      },
+      (err) => {
+        console.warn(err)
+        dispatch({action: "add", body: {msg: 'Ad deletion failure', good: false}});
+      }
+    )
+  }
+
+  const refreshData = () => {
+    console.log('refreshData called')
+    // setData
+  }
 
   return (
     <>
-
+      <MyPostsCard callbackDelete={handleCallbackDelete}/>
     </>
   );
 }
--- src/components/myPosts/MyPostsCard.js
+++ src/components/myPosts/MyPostsCard.js
@@ -1,20 +1,75 @@
 import './MyPostsCard.css';
 
 const fakeAd = {
-  id: 1, adType: 'textbook', adTitle: 'This is a fake title for an ad but this is very long', price: '1233425',
+  id: 1, adType: 'textbook', adTitle: 'Tttthis is a fake title for an ad but this is very long', price: '1233425',
   comment: 'This is a fake comment for and ad but this is very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long very long',
   createTime: "Apr 4, 2022, 12:00:00 AM", numberOfTaps: 99, imageIds: '3,6,9'
 }
 
-function MyPostsCard() {
+const fakeAd2 = {
+  id: 1, adType: 'textbook', adTitle: 'this is a fake title', price: '123',
+  comment: 'This is a fake comment for and ad bery long very long very long very long very long',
+  createTime: "Apr 4, 2022, 12:00:00 AM", numberOfTaps: 66, imageIds: '3,6,9'
+}
 
+function MyPostsCard({
+  adData=fakeAd2,
+  callbackEdit=null,
+  callbackDelete=null
+                     }) {
 
+  const handleEdit = () => {
+    callbackEdit(adData.id);
+  }
 
-
+  const handleDelete = () => {
+    callbackDelete(adData.id)
+  }
 
   return (
     <div className={'MyPostsCard card'}>
+      <div className={'rows-container'}>
 
+        <div className={'row-thumbnail'}>
+          <img src="./default_cover.jpg" alt=""/>
+        </div>
+
+        <hr/>
+
+        <div className={'row-title'}>
+          <p>{adData.adTitle}</p>
+        </div>
+
+        <div className={'row-price'}>
+          <p>{adData.price} RMB</p>
+        </div>
+
+        <div className={'row-time row-price'}>
+          <p>{adData.createTime}</p>
+        </div>
+
+        <div className={'row-tapped-by row-price'}>
+          <p>Tapped by {'sh2013'}</p>
+        </div>
+
+        <hr/>
+
+        <div className={'row-buttons'}>
+          <div className={'btn-edit'}>
+            <p className={'clickable'} onClick={handleEdit}>edit</p>
+          </div>
+          <div className={'btn-delete'}>
+            <p className={'clickable'} onClick={handleDelete}>delete</p>
+          </div>
+        </div>
+
+      </div>
+
+      
+      
+      
+      
+      
     </div>
   );
 }
