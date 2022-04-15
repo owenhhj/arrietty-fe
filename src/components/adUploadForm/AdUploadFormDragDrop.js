3a96df4eda0e8c03e00b9410c99e36f6421be4a0
h <juanjuanjks@163.com>
2022-04-15 23:42:47 +0800

    Promise: VERY VERY BAD APPROACH, push --> Robert fix

--- src/components/adUploadForm/AdUploadFormDragDrop.js
+++ src/components/adUploadForm/AdUploadFormDragDrop.js
@@ -1,8 +1,7 @@
 import "./AdUploadFormDragDrop.css"
 import AdUploadFormDragDropPic from "./AdUploadFormDragDropPic";
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 import {useEffect, useState} from 'react'
-import {dataFetch} from "../common/common";
 
 
 // TODO release RAM from createObjectURL after submission
@@ -13,30 +12,75 @@ function AdUploadFormDragDrop({
                   }) {
   const ROOT = 'https://localhost:8000/';
   const [picURLs, setPicURLs] = useState([]);
-  let pic = [];  // current pic being added
-  // const reader = new FileReader();
+  // let pic = [];  // current pic being added
 
   useEffect(() => {
     let ids = imageIdsOriginal.split(',');
-    // let fetchCount = 0;
     console.log('imageIdsOriginal:', ids);
-    ids.forEach((id) => {
+    // ids.forEach((id) => {
+    //   fetch(`${ROOT}image?id=${id}`)
+    //     .then(async res => {
+    //       let imgBlob = res.blob();
+    //       let imgFile = new File([await imgBlob], `adImgOri${id}`);
+    //       let tempFile = {
+    //         'name': `adImgOri${id}`,
+    //         'url': URL.createObjectURL(imgFile),
+    //         'file': imgFile
+    //       };
+    //       handlePicAdd([tempFile]);
+    //     });
+    //
+    // });
 
-      fetch(`${ROOT}image?id=${id}`)
-        .then(async res => {
-          let imgBlob = res.blob();
-          let imgFile = new File([await imgBlob], `adImageOriginal${id}`);
-          let tempFile = {
-            'name': `adImageOriginal${id}`,
-            'url': URL.createObjectURL(imgFile),
-            'file': imgFile
-          };
-          pic.push(tempFile);
-          // fetchCount++;
-          handlePicAdd();
-        })
-    });
+    // console.log('handleFetchImgOriginal returned:', handleFetchImgOriginal(ids));
+    handleFetchImgOriginal(ids);
+
   }, []);
+
+  useEffect(() => {console.log('useEffect picURLs changed to:',picURLs)}, [picURLs]);
+
+  const handleFetchImgOriginal = (ids) => {
+    let tempPics = [];
+    let temp = ids.map(async id => {
+      // return await fetch(`${ROOT}image?id=${id}`)
+      //   .then(async res => {
+      //     let imgBlob = await res.blob();
+      //     let imgFile = new File([imgBlob], `adImgOri${id}`);
+      //     return {
+      //       'name': `adImgOri${id}`,
+      //       'url': URL.createObjectURL(imgFile),
+      //       'file': imgFile
+      //     };
+      //   });
+
+      // tempPics.push(await fetch(`${ROOT}image?id=${id}`)
+      //   .then(async res => {
+      //     let imgBlob =  res.blob();
+      //     let imgFile = new File([await imgBlob], `adImgOri${id}`);
+      //     return {
+      //       'name': `adImgOri${id}`,
+      //       'url': URL.createObjectURL(imgFile),
+      //       'file': imgFile
+      //     }
+      //   }));
+
+      let res = await fetch(`${ROOT}image?id=${id}`);
+      let imgFile = new File([await res.blob()], `adImgOri${id}`);
+      let tempPic = {
+        'name': `adImgOri${id}`,
+        'url': URL.createObjectURL(imgFile),
+        'file': imgFile
+      };
+      tempPics.push(tempPic);
+      console.log(tempPic)
+    });
+    tempPics.forEach(tp => console.log('tp.file', tp.file));
+    console.log('handleFetchImgOriginal Promise mapped tempPics:', tempPics);
+    handlePicAdd(tempPics);
+    // return temp;
+    // console.log('handleFetchImgOriginal Promise mapped temp:', temp);
+    // handlePicAdd(temp);
+  }
 
   const toParent = (v) => {
     onChange(identifier, v);
@@ -44,38 +88,46 @@ function AdUploadFormDragDrop({
 
   const picInputChange = (e) => {
     e.preventDefault();
+    let currPics = [];
     if (e.target.files && e.target.files[0]) {
       for (let i=0; i<e.target.files.length; i++) {
         // TODO upload files .jpg/png check
-        pic.push({
+        currPics.push({
           "name": e.target.files[i].name,
           "url": URL.createObjectURL(e.target.files[i]),
           "file": e.target.files[i]
         });
       }
     }
-    handlePicAdd();
+    handlePicAdd(currPics);
   }
 
   // fixme why not just pass in pic[]
-  const handlePicAdd = () => {
+  const handlePicAdd = (currPics) => {
     console.log('handlePicAdd picURLs[]:', picURLs);
-    console.log('handlePicAdd pic[]:', pic);
-    // let legal = pic.length > 0;  // fixme length===0 should work, right?
-    let legal = true;
-    picURLs.forEach((uploaded) => {
-      pic.forEach((newPic) => {
-        if (newPic.name === uploaded.name) {
-          legal = false;
-        }
-      })
-    })
-    if (legal) {
-      toParent([...picURLs, ...pic].map((item) => (item.file)));
-      setPicURLs([...picURLs, ...pic]);
-      pic = [];
+    console.log('handlePicAdd currPics[]:', currPics);
+
+    // let legal = true;
+    // picURLs.forEach((uploaded) => {
+    //   currPics.forEach((newPic) => {
+    //     if (newPic.name === uploaded.name) {
+    //       legal = false;
+    //     }
+    //   })
+    // });
+    console.log('before legal:', currPics);
+    // todo thread interleaving??? use a outside variable? probably not --> ensure all fetches are done
+    if (true) {
+      console.log('handlePicAdd legal currPics before temp:', currPics);
+      console.log('handlePicAdd legal picURLs before temp:', picURLs);
+      // let temp = [...picURLs, ...currPics];
+      let temp = picURLs.concat(currPics);
+      console.log('handlePicAdd legal temp before setState:', temp)
+      console.log('handlePicAdd legal picURLs before setState:', picURLs);
+      toParent(temp.map((item) => (item.file)));
+      // setPicURLs(temp);
+      setPicURLs(currPics);
     } else {
-      pic = [];
       alert('Upload another picture!');
     }
   }
