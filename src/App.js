8bf37bd96be3266f2d6231a5ece74e069f3d2755
h <juanjuanjks@163.com>
2022-04-17 11:41:09 +0800

    Navbar: starting to add red dots

--- src/App.js
+++ src/App.js
@@ -10,25 +10,24 @@ import RouteMyPosts from "./components/RouteMyPosts";
 import RouteNotification from "./components/RouteNotification";
 import Modal from "react-modal";
 
-
-
 function App() {
-    Modal.setAppElement('#root');
+  Modal.setAppElement('#root');
 
   return (
     <div className="App" id={"app-root"}>
 
-        <BrowserRouter>
-            <Navbar isAdmin={true} />
-            <Routes>
-                <Route path="/home" element={<RouteHome/>} exact/>
-                <Route path="/myPosts" element={<RouteMyPosts/>} exact/>
-                <Route path="/admin" element={<RouteAdmin/>} exact/>
-                <Route path="/notification" element={<RouteNotification/>} exact/>
-                <Route path="/favorite" element={<RouteFavorite/>} exact/>
+      <BrowserRouter>
+        <Navbar isAdmin={true}/>
+        <Routes>
+          {/*<Route path="/" element={<RouteHome/>} exact/>*/}
+          <Route path="/home" element={<RouteHome/>} exact/>
+          <Route path="/myPosts" element={<RouteMyPosts/>} exact/>
+          <Route path="/admin" element={<RouteAdmin/>} exact/>
+          <Route path="/notification" element={<RouteNotification/>} exact/>
+          <Route path="/favorite" element={<RouteFavorite/>} exact/>
 
-            </Routes>
-        </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
 
 
     </div>
