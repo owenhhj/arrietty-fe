7664b79a95ed988330e7ed853f65f95bc7d1faaf
h <juanjuanjks@163.com>
2022-04-29 00:05:09 +0800

    bugfix: refactored new ad button

--- src/components/myProfile/MyProfile.js
+++ src/components/myProfile/MyProfile.js
@@ -6,18 +6,44 @@ import {showGeneralNoti} from "../common/GeneralNotiProvider";
 import {useState, useEffect} from "react";
 import {dataFetch} from "../common/common";
 import {getSiteInfo} from "../common/SiteInfoProvider";
+import Modal from "react-modal";
+import AdUploadForm from "../adUploadForm/AdUploadForm";
 
-function MyProfile({callback}) {
+const customStyles = {
+  content: {
+    position: "absolute",
+    left:0,
+    top:"1rem",
+    width:"100vw",
+    height:"calc(100vh-2rem)",
+    "overflow-y": "scroll",
+    background: "transparent",
+    display: "flex",
+    "justify-content":"center",
+    border:"none",
+  },
+};
+
+function MyProfile() {
   const ROOT = 'https://localhost:8000/';
   const [myProfileData, setMyProfileData] = useState(getSiteInfo());
   const [avatarSrc, setAvatarSrc] = useState(  // pass state hook between components
     myProfileData.avatarImageId ? `${ROOT}image?id=${myProfileData.avatarImageId}` : "./default_avatar.jpg"
   );
   const [pageShow, setPageShow] = useState(0);  // 0: disp, 1: edit
+  const [showNewAdForm, setShowNewAdForm] = useState(false);
 
   useEffect(() => {
     refreshData();
   }, []);
+
+  useEffect(() => {
+    if (showNewAdForm) {
+      document.getElementById("app-root").style.filter = 'blur(2px)';
+    } else {
+      document.getElementById("app-root").style.filter = 'blur(0px) grayscale(0%)';
+    }
+  }, [showNewAdForm]);
 
   const dispatch = showGeneralNoti();
   const handleShowNoti = (msg, good) => {
@@ -45,22 +71,28 @@ function MyProfile({callback}) {
     } else if (data.action === "switch") {
       setPageShow(1-pageShow)
     } else if (data.action === "addNewAd") {
-      callback(true);
+      // callback(true);
+      setShowNewAdForm(true);
     }
   }
 
   return (
-    <div className="MyProfile card non-text">
-      {pageShow === 0 && (
-        <MyProfileDisplay data={myProfileData} avatarSrc={avatarSrc} callback={callbackHandler}/>
-      )}
-      {pageShow === 1 && (
-        <MyProfileEdit
-          profilePrev={myProfileData} avatarSrc={avatarSrc} setAvatarSrc={setAvatarSrc}
-          callback={callbackHandler}
-        />
-      )}
-    </div>
+    <>
+      <div className="MyProfile card non-text">
+        {pageShow === 0 && (
+          <MyProfileDisplay data={myProfileData} avatarSrc={avatarSrc} callback={callbackHandler}/>
+        )}
+        {pageShow === 1 && (
+          <MyProfileEdit
+            profilePrev={myProfileData} avatarSrc={avatarSrc} setAvatarSrc={setAvatarSrc}
+            callback={callbackHandler}
+          />
+        )}
+      </div>
+      <Modal isOpen={showNewAdForm} style={customStyles}>
+        <AdUploadForm callback={setShowNewAdForm}/>
+      </Modal>
+    </>
   );
 }
 
